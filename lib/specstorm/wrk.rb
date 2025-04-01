f97d71a97b1eb6e287938d4153342bece868e0c8
Daniel Westendorf <daniel@prowestech.com>
2025-04-01 08:21:09 -0600

    Generate gem

---
+++ lib/specstorm/wrk.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "wrk/version"
+
+module Specstorm
+  module Wrk
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/specstorm/wrk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Specstorm::Wrk do
+  it "has a version number" do
+    expect(Specstorm::Wrk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
